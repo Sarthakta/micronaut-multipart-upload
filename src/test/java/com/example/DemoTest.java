a93cba459c20c6a14c31357e9f3fd111576ca22c
vyasamit <amit.vyas.in@gmail.com>
2020-06-23 15:59:30 -0500

    Test Case Added.

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -1,21 +1,40 @@
 package com.example;
 
-import io.micronaut.runtime.EmbeddedApplication;
-import io.micronaut.test.annotation.MicronautTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.multipart.MultipartBody;
+import io.micronaut.test.annotation.MicronautTest;
 import javax.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 @MicronautTest
 public class DemoTest {
 
-    @Inject
-    EmbeddedApplication application;
+  @Inject
+  @Client("/")
+  HttpClient client;
 
-    @Test
-    void testItWorks() {
-        Assertions.assertTrue(application.isRunning());
-    }
+  @Test
+  void multiPartAttachmentWithJsonDataUpload() {
+
+    MultipartBody body = MultipartBody.builder()
+        .addPart("email", "email.json", MediaType.APPLICATION_JSON_TYPE,
+            "{\"recipient\":\"amit_vyas@t.com\",\"subject\":\"Test Email Service\",\"htmlBody\":\"My First TeXt EmAiL\"}"
+                .getBytes())
+        .addPart("attachment", "amazon", MediaType.TEXT_PLAIN_TYPE, new byte[0])
+        .build();
+
+    HttpResponse response = client.toBlocking().exchange(HttpRequest.POST("/mail/attachment", body)
+        .contentType(MediaType.MULTIPART_FORM_DATA_TYPE));
+
+    Assertions.assertEquals(response.getStatus(), HttpStatus.OK);
+
+  }
 
 }
